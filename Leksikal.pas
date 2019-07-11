0d57da3640055e8997c39057f1b2a809c0ef224c
DIKI SUPRIADI <dikisupriadi021@email.unikom.ac.id>
2019-07-11 15:32:54 +0700

    only leksikal

---
+++ Leksikal.pas
@@ -0,0 +1,498 @@
+//program ini di buat dengan menggunakan fpc free pascal dan dibuat dengan texteditor visual studio code
+program Leksikal;
+Uses sysutils;
+type
+    rec_token = record
+        token: char;
+        category: string;
+    end;    
+var 
+    ch : char;
+    theFile : File of char;
+    i : integer;
+    sizeOfChar:integer;
+    token: array of Char;
+    totalToken:integer;
+   
+
+    intcon,realcon,charcon,stringt,notsy,plus,minus,times,idiv,rdiv,imod,andsy,orsy,egl,neg,gtr,geg,lss,leg,lparent,rparent,lbrack,rbrack,comma,semicolon,period,colon,becomes,constsy,typesy,varsy,functionsy,proceduresy,arraysy,recordsy,programsy,ident,beginsy,ifsy,casesy,repeatsy,whilesy,forsy,endsy,elsesy,untilsy,ofsy,dosy,tosy,downtosy,thensy,typeint,typechar,typereal : integer;
+
+    function countChar():integer;
+    var
+        z:integer;
+    begin
+        Assign (theFile,'hitung_gaji.pas');
+        Reset (theFile); 
+        ch:='A';
+        z:=0;
+        While not Eof(theFile) do
+            Begin
+                Read (theFile,ch);
+                sizeOfChar:=sizeOfChar+1;
+            end;
+        close(theFile);
+        countChar:=sizeOfChar;
+    end;
+    procedure charLoad(var token: array of Char);
+    var z:Integer;
+    begin
+        Assign (theFile,'hitung_gaji.pas');
+        Reset (theFile); 
+        ch:='A';
+        z:=0;
+        While not Eof(theFile) do
+            Begin
+                Read (theFile,ch);
+                token[z]:=ch;
+                z+=1;
+            end;
+        close (theFile);
+    end;
+
+
+begin
+    intcon:= 0;realcon:=0;charcon:=0;stringt:=0;notsy:=0;plus:=0;minus:=0;times:=0;idiv:=0;rdiv:=0;imod:=0;
+    andsy:=0;orsy:=0;egl:=0;neg:=0;gtr:=0;geg:=0;lss:=0;leg:=0;lparent:=0;rparent:=0;lbrack:=0;comma:=0;rbrack:=0;
+    semicolon:=0;period:=0;colon:=0;becomes:=0;constsy:=0;typesy:=0;varsy:=0;functionsy:=0;proceduresy:=0;
+    arraysy:=0;recordsy:=0;programsy:=0;ident:=0;beginsy:=0;ifsy:=0;casesy:=0;repeatsy:=0;whilesy:=0;
+    forsy:=0;endsy:=0;elsesy:=0;untilsy:=0;ofsy:=0;dosy:=0;tosy:=0;downtosy:=0;thensy:=0;typeint:=0;
+    typechar:=0;typereal:=0;
+    
+    SetLength(token,countChar());
+    charLoad(token);
+    i:=-1;
+    writeln('Scanner list');
+    for i:=0 to countChar do
+     begin
+
+        if  (UpperCase(token[i])='I') then 
+           if (UpperCase(token[i+1])='N') then 
+           if (UpperCase(token[i+2])='T') then 
+           if (UpperCase(token[i+3])='E') then 
+           if (UpperCase(token[i+4])='G') then
+           if (UpperCase(token[i+5])='E') then
+           if (UpperCase(token[i+6])='R') then
+            begin
+                intcon +=1;
+                writeln('tipe data integer');
+            end
+        else
+        if  (UpperCase(token[i+0])='R') then 
+          if  (UpperCase(token[i+1])='E') then 
+          if  (UpperCase(token[i+2])='A') then 
+           if (UpperCase(token[i+3])='L') then
+          begin
+                realcon+=1;
+                writeln('tipe data real');
+            end
+        else
+           
+        if (UpperCase(token[i+0])='C') then 
+          if  (UpperCase(token[i+1])='H') then 
+           if (UpperCase(token[i+2])='A') then 
+          if  (UpperCase(token[i+3])='R') then
+              begin
+                charcon += 1;
+                writeln('tipe data char');
+            end
+        else
+            
+        if  (UpperCase(token[i+0])='S') then 
+          if  (UpperCase(token[i+1])='T') then 
+           if (UpperCase(token[i+2])='R') then 
+           if (UpperCase(token[i+3])='I') then 
+            if  (UpperCase(token[i+4])='N') then
+            if  (UpperCase(token[i+5])='G') then
+                writeln('tipe data string');
+           
+        if  (UpperCase(token[i+0])='N') then 
+          if  (UpperCase(token[i+1])='O') then 
+           if (UpperCase(token[i+2])='T') then
+            
+                writeln('operator not');
+            
+        if  (UpperCase(token[i+0])='+') then
+            begin
+                plus += 1;
+                writeln('operator +');
+            end
+        else
+        if  (UpperCase(token[i+0])='-') then
+            begin
+                minus += 1;
+                writeln('operator -');
+            end
+        else    
+        if  (UpperCase(token[i+0])='*') then
+            begin
+                times += 1;
+                writeln('operator *');
+            end
+        else
+        if  (UpperCase(token[i+0])='D') and 
+            (UpperCase(token[i+1])='I') and 
+            (UpperCase(token[i+2])='V') then
+            begin
+                idiv += 1;
+                writeln('operator div');
+            end
+        else
+        if  (UpperCase(token[i+0])='/') then 
+            begin
+                rdiv += 1;
+                writeln('operator /');
+            end
+        else
+        if  (UpperCase(token[i+0])='M') and 
+            (UpperCase(token[i+1])='O') and 
+            (UpperCase(token[i+2])='D') then
+            begin
+                imod += 1;
+                writeln('operator mod');
+            end
+        else
+        if  (UpperCase(token[i+0])='A') and 
+            (UpperCase(token[i+1])='N') and 
+            (UpperCase(token[i+2])='D') then
+            begin
+                andsy += 1;
+                writeln('operator and');
+            end
+        else
+        if  (UpperCase(token[i+0])='O') and 
+            (UpperCase(token[i+1])='R') then
+            begin
+                orsy += 1;
+                writeln('operator or');
+            end
+        else
+        if  (UpperCase(token[i+0])='=') then
+            begin
+                egl+=1;
+                writeln('operator =');
+            end
+        else
+        if  (UpperCase(token[i+0])='<') then
+            begin
+                if (UpperCase(token[i+1])='=') then
+                 begin
+                    writeln('operator <=');
+                    leg+=1;
+                 end else if(UpperCase(token[i+1])='>') then
+                   begin
+                     writeln('operator <>');
+                     neg +=1;
+                   end else 
+                        lss+=1;
+                        writeln('operator <');
+            end
+        else 
+        if  (UpperCase(token[i+0])='(') then
+            begin
+                lparent += 1;
+                writeln('operator (');
+            end
+        else  
+        if  (UpperCase(token[i+0])=')') then
+            begin
+                rparent += 1;
+                writeln('operator )');
+            end
+        else 
+        if  (UpperCase(token[i+0])='>') then
+            begin
+                if (UpperCase(token[i+1])='=') then
+                 begin
+                    writeln('operator >=');
+                    geg+=1;
+                 end else 
+                     gtr +=1;
+                     writeln('operator >');
+            end
+        else 
+        if  (UpperCase(token[i+0])='[') then
+            begin
+                lbrack += 1;
+              writeln('simbol [');
+            end
+        else 
+        if  (UpperCase(token[i+0])=']') then
+            begin
+                rbrack += 1;
+              writeln('simbol ]');
+            end
+        else
+        if  (UpperCase(token[i+0])=',') then
+            begin
+            comma +=1;
+              writeln('simbol ,');
+            end
+        else
+        if  (UpperCase(token[i+0])=';') then
+            begin
+                semicolon += 1;
+              writeln('simbol ;');
+            end
+        else
+        if  (UpperCase(token[i+0])='.') then
+            begin
+                period += 1;
+              writeln('simbol .');
+            end
+        else
+        if  (UpperCase(token[i+0])=':') then
+            begin
+              if (UpperCase(token[i+1])='=') then
+                begin
+                  writeln('operator :=');
+                  becomes +=1;
+                end else 
+                    colon += 1;
+                    writeln('operator :');
+            end
+        else
+        if  (UpperCase(token[i+0])='C') then 
+          if  (UpperCase(token[i+1])='O') then 
+          if  (UpperCase(token[i+2])='N') then 
+          if  (UpperCase(token[i+3])='S') then 
+          if  (UpperCase(token[i+4])='T') then
+            
+                writeln('keyword const');
+            
+        if  (UpperCase(token[i])='V') and 
+            (UpperCase(token[i+1])='A') and 
+            (UpperCase(token[i+2])='R') then
+            begin
+                varsy +=1;
+                writeln('keyword var');
+            end
+        else
+        if  (UpperCase(token[i+0])='F') then 
+          if  (UpperCase(token[i+1])='U') then 
+          if  (UpperCase(token[i+2])='N') then 
+           if (UpperCase(token[i+3])='C') then 
+           if (UpperCase(token[i+4])='T') then
+           if (UpperCase(token[i+5])='I') then 
+           if (UpperCase(token[i+5])='O') then  
+           if (UpperCase(token[i+6])='N') then
+            begin
+            functionsy +=1;
+                writeln('keyword function');
+            end
+        else 
+        if  (UpperCase(token[i+0])='P') then 
+          if  (UpperCase(token[i+1])='R') then 
+           if (UpperCase(token[i+2])='O') then 
+           if (UpperCase(token[i+3])='C') then 
+           if (UpperCase(token[i+4])='E') then
+           if (UpperCase(token[i+5])='D') then 
+           if (UpperCase(token[i+5])='U') then  
+          if  (UpperCase(token[i+6])='R') then  
+           if (UpperCase(token[i+7])='E') then
+            begin
+                proceduresy +=1;
+                writeln('keyword procedure');
+            end
+        else  
+        if  (UpperCase(token[i+0])='A') then 
+          if  (UpperCase(token[i+1])='R') then 
+          if  (UpperCase(token[i+2])='R') then 
+          if  (UpperCase(token[i+3])='A') then 
+          if  (UpperCase(token[i+4])='Y') then
+            begin
+                arraysy +=1;
+                writeln('keyword array');
+            end
+        else  
+        if  (UpperCase(token[i+0])='R') then 
+          if  (UpperCase(token[i+1])='E') then 
+          if  (UpperCase(token[i+2])='C') then 
+          if  (UpperCase(token[i+3])='O') then 
+          if  (UpperCase(token[i+4])='R') then
+           if (UpperCase(token[i+5])='D') then
+            begin
+                period +=1;
+                writeln('keyword record');
+            end
+        else  
+        if  (UpperCase(token[i])='P') then 
+          if  (UpperCase(token[i+1])='R') then 
+           if (UpperCase(token[i+2])='O') then 
+           if (UpperCase(token[i+3])='G') then 
+           if (UpperCase(token[i+4])='R') then
+           if (UpperCase(token[i+5])='A') then 
+           if (UpperCase(token[i+6])='M') then
+            begin
+                programsy +=1;
+                writeln('keyword program');
+            end  
+        else   
+        if  (UpperCase(token[i+0])='B') then 
+          if  (UpperCase(token[i+1])='E') then 
+          if  (UpperCase(token[i+2])='G') then 
+           if (UpperCase(token[i+3])='I') then 
+           if (UpperCase(token[i+4])='N') then
+            begin
+                beginsy +=1;
+                writeln('keyword begin');
+            end
+        else    
+        if  (UpperCase(token[i+0])='I') and 
+            (UpperCase(token[i+1])='F') then
+            begin
+                ifsy +=1;
+                writeln('keyword if');
+            end
+        else   
+        if  (UpperCase(token[i+0])='C') then 
+          if  (UpperCase(token[i+1])='A') then 
+         if  (UpperCase(token[i+2])='S') then 
+           if (UpperCase(token[i+3])='E') then
+            begin
+                casesy +=1;
+                writeln('keyword case');
+            end
+        else  
+        if  (UpperCase(token[i+0])='R') then 
+           if (UpperCase(token[i+1])='E') then 
+          if  (UpperCase(token[i+2])='P') then 
+           if (UpperCase(token[i+3])='E') then 
+           if (UpperCase(token[i+4])='A') then
+           if (UpperCase(token[i+5])='T') then
+            begin
+                repeatsy +=1;
+                writeln('keyword repeat');
+            end
+        else    
+        if  (UpperCase(token[i+0])='W') then 
+           if (UpperCase(token[i+1])='H') then 
+           if (UpperCase(token[i+2])='I') then 
+           if (UpperCase(token[i+3])='L') then 
+           if (UpperCase(token[i+4])='E') then
+            begin
+                whilesy +=1;
+                writeln('keyword while');
+            end
+        else   
+           
+        if  (UpperCase(token[i+0])='E') then 
+           if (UpperCase(token[i+1])='N') then 
+          if  (UpperCase(token[i+2])='D') then
+            begin
+                endsy +=1;
+                writeln('keyword end');
+            end
+        else    
+        if  (UpperCase(token[i+0])='E') then 
+          if  (UpperCase(token[i+1])='L') then 
+           if (UpperCase(token[i+2])='S') then 
+           if (UpperCase(token[i+3])='E') then
+            begin
+                elsesy +=1;
+                writeln('keyword else');
+            end
+        else    
+        if  (UpperCase(token[i+0])='U') then 
+           if (UpperCase(token[i+1])='N') then 
+           if (UpperCase(token[i+2])='T') then 
+           if (UpperCase(token[i+3])='I') then 
+           if (UpperCase(token[i+4])='L') then
+            
+                writeln('keyword until');
+        if  (UpperCase(token[i+0])='O') and 
+            (UpperCase(token[i+1])='F') then
+            begin
+                ofsy +=1;
+                writeln('keyword of');
+            end
+        else     
+        if  (UpperCase(token[i+0])='D') and 
+            (UpperCase(token[i+1])='O') then
+            begin
+                dosy +=1;
+                writeln('keyword do');
+            end
+        else    
+        if  (UpperCase(token[i+0])='T') and 
+            (UpperCase(token[i+1])='O') then
+            begin
+                tosy +=1;
+                writeln('keyword to');
+            end
+        else  
+        if  (UpperCase(token[i+0])='D') then 
+         if   (UpperCase(token[i+1])='O') then 
+          if  (UpperCase(token[i+2])='W') then 
+          if  (UpperCase(token[i+3])='N') then 
+           if (UpperCase(token[i+4])='T') then
+          if  (UpperCase(token[i+5])='O') then
+            
+                writeln('keyword downto');
+              
+        if  (UpperCase(token[i+0])='T') then 
+          if  (UpperCase(token[i+1])='H') then 
+           if (UpperCase(token[i+2])='E') then 
+          if  (UpperCase(token[i+3])='N') then
+          
+                writeln('keyword then');
+            
+    // i:=i+1;
+    end;//endfor
+    writeln();
+    writeln();
+    totalToken := intcon + realcon + charcon + stringt + notsy + plus + minus + times + idiv + rdiv + imod + andsy + orsy + egl + neg + gtr + geg + lss + leg + lparent + rparent + lbrack + rbrack + comma + semicolon + period + colon + becomes + constsy + typesy + varsy + functionsy + proceduresy + arraysy + recordsy + programsy + ident + beginsy + ifsy + casesy + repeatsy + whilesy + forsy + endsy + elsesy + untilsy + ofsy + dosy + tosy + downtosy + thensy + typeint + typechar + typechar;
+
+    writeln('intcon :',intcon);
+    writeln('realcon :',realcon);
+    writeln('charcon :',charcon);
+    writeln('stringt :',stringt);
+    writeln('notsy:',notsy);
+    writeln('plus :',plus);
+    writeln('minus :',minus);
+    writeln('times :',times);
+    writeln('idiv :',idiv);
+    writeln('rdiv :',rdiv);
+    writeln('imod :',imod);
+    writeln('andsy :',andsy);
+    writeln('orsy :',orsy);
+    writeln('egl :',egl);
+    writeln('neg :',neg);
+    writeln('gtr :',gtr);
+    writeln('geg :',geg);
+    writeln('lss :',lss);
+    writeln('leg :',leg);
+    writeln('lparent :',lparent);
+    writeln('lbrack :',lbrack);
+    writeln('rbrack :',rbrack);
+    writeln('comma :',comma);
+    writeln('semicolon :',semicolon);
+    writeln('colon :',colon);
+    writeln('becomes :',becomes);
+    writeln('constsy :',constsy);
+    writeln('typesy :',typesy);
+    writeln('varsy :',varsy);
+    writeln('functionsy :',functionsy);
+    writeln('proceduresy :',proceduresy);
+    writeln('arraysy :',arraysy);
+    writeln('recordsy :',recordsy);
+    writeln('programsy :',programsy);
+    writeln('identifier :',ident);
+    writeln('beginsy :',beginsy);
+    writeln('ifsy :',ifsy);
+    writeln('casesy :',casesy);
+    writeln('whilesy :',whilesy);
+    writeln('forsy :',forsy);
+    writeln('endsy :',endsy);
+    writeln('elsesy :',elsesy);
+    writeln('untilsy :',untilsy);
+    writeln('ofsy :',ofsy);
+    writeln('dosy :',dosy);
+    writeln('tosy :',tosy);
+    writeln('downtosy :',downtosy);
+    writeln('typeint :',typeint);
+    writeln('typechar :',typechar);
+    writeln('typereal :',typereal);
+ Writeln;
+ readln();
+
+end.
